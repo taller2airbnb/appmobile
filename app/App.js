a5bf1408919376b51b76039baa1ce550c6fcb083
cordobacesar <cordobacesar@gmail.com>
2020-10-22 18:42:55 -0300

    Check endpoints

--- app/App.js
+++ app/App.js
@@ -1,28 +1,122 @@
+// React Native Fetch – To make HTTP API call in React Native
+// https://aboutreact.com/react-native-http-networking/
+
+// import React in our code
 import React from 'react';
-import { View, Text, StyleSheet, ImageBackground, ScrollView, TextInput, Button } from 'react-native';
+
+// import all the components we are going to use
+import {
+  SafeAreaView,
+  StyleSheet,
+  View,
+  TouchableOpacity,
+  Text,
+} from 'react-native';
 
 const App = () => {
-  return (
-    
-      <View style={{
-        justifyContent: "center",
-        alignItems: "center"
-      }}>
-      <ImageBackground source={{
-            uri: 'https://www.laprensa.hn/csp/mediapool/sites/dt.common.streams.StreamServer.cls?STREAMOID=THjb9uHQe4McutbIodzgps$daE2N3K4ZzOUsqbU5sYtgFRLkufOsRgvpd5rThyvBWCsjLu883Ygn4B49Lvm9bPe2QeMKQdVeZmXF$9l$4uCZ8QDXhaHEp3rvzXRJFdy0KqPHLoMevcTLo3h8xh70Y6N_U_CryOsw6FTOdKL_jpQ-&CONTENTTYPE=image/jpeg',
-          }} style={{width: '100%', height: '100%'}}>
+  const getDataProfile = () => {
+    //GET request
+    fetch('https://taller2airbnb-profile.herokuapp.com/health', {
+      method: 'GET',
+      //Request Type
+    })
+      .then((response) => response.json())
+      //If response is in json then in success
+      .then((responseJson) => {
+        //Success
+        alert(JSON.stringify(responseJson));
+        console.log(responseJson);
+      })
+      //If response is not in json then in error
+      .catch((error) => {
+        //Error
+        alert(JSON.stringify(error));
+        console.error(error);
+      });
+  };
 
-      <Text style= {{
-        color: 'white',
-        fontWeight: 'bold',
-        fontSize: 50,
-        textAlign:"center",
-        margin: 20
-      }}>Hello World!</Text>
-      </ImageBackground>
-      
+  const getDataBusinessCore = () => {
+    //GET request
+    fetch('https://taller2airbnb-businesscore.herokuapp.com/health', {
+      method: 'GET',
+      //Request Type
+    })
+      .then((response) => response.json())
+      //If response is in json then in success
+      .then((responseJson) => {
+        //Success
+        alert(JSON.stringify(responseJson));
+        console.log(responseJson);
+      })
+      //If response is not in json then in error
+      .catch((error) => {
+        //Error
+        alert(JSON.stringify(error));
+        console.error(error);
+      });
+  };
+
+  return (
+    <SafeAreaView style={{flex: 1}}>
+      <View style={styles.container}>
+        <View style={styles.container}>
+          {/*Running GET Request*/}
+          <TouchableOpacity
+            style={styles.buttonStyle}
+            onPress={getDataProfile}>
+            <Text style={styles.textStyle}>
+              Get status PROFILING CORE
+            </Text>
+          </TouchableOpacity>
+          {/*Running POST Request*/}
+          <TouchableOpacity
+            style={styles.buttonStyle}
+            onPress={getDataBusinessCore}>
+            <Text style={styles.textStyle}>
+              Get status BUSINESS CORE
+            </Text>
+          </TouchableOpacity>
+        </View>
+        <Text
+          style={{
+            fontSize: 18,
+            textAlign: 'center',
+            color: 'grey'
+          }}>
+          Marche ese AirBnB
+          {'\n'}
+          Checkpoint 1 - Taller II
+        </Text>
+        <Text
+          style={{
+            fontSize: 16,
+            textAlign: 'center',
+            color: 'grey'
+          }}>
+          Number One Group
+        </Text>
       </View>
+    </SafeAreaView>
   );
-}
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+    justifyContent: 'center',
+    padding: 20,
+  },
+  textStyle: {
+    fontSize: 18,
+    color: 'white',
+  },
+  buttonStyle: {
+    alignItems: 'center',
+    backgroundColor: 'black',
+    padding: 10,
+    marginVertical: 10,
+  },
+});
 
 export default App;
