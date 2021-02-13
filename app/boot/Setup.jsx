506ffb615c211eee4cc09c065aed16385ae3d259
martinchary <martochary@gmail.com>
2021-02-13 16:34:03 -0300

    removed firebase config, check confluence to add it when making the builds

--- app/boot/Setup.jsx
+++ app/boot/Setup.jsx
@@ -9,13 +9,13 @@ import variables from "../theme/variables/commonColor";
 import firebase from "firebase/app";
 
 const FirebaseConfig = {
-  apiKey: "AIzaSyDM2NPGRMQspGMEv2znm0kOuBL3iWOzPWI",
-  appId: "1:481615734249:android:4b1c1c33582e15be4a9778",
-  projectId: "bookbnb-degoas-ed",
-  authDomain: "bookbnb-degoas-ed.firebaseapp.com",
-  databaseURL: "https://bookbnb-degoas-ed.firebaseio.com",
-  storageBucket: "bookbnb-degoas-ed.appspot.com",
-  messagingSenderId: "481615734249",
+  apiKey: "",
+  appId: "",
+  projectId: "",
+  authDomain: "",
+  databaseURL: "",
+  storageBucket: "",
+  messagingSenderId: "",
 }
 
 firebase.initializeApp(FirebaseConfig);
