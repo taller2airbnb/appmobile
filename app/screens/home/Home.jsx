33cf06e56d7242407727501da14d6e0355f2ff94
santiw <santiago.weber91@gmail.com>
2021-02-27 19:08:58 -0300

    degoas y accept y reject

--- app/screens/home/Home.jsx
+++ app/screens/home/Home.jsx
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
-import { ImageBackground, View, StatusBar } from "react-native";
+import { ImageBackground, View, StatusBar, Dimensions } from "react-native";
 import { Container, Button, H3, Text } from "native-base";
 
 import styles from "./styles";
 
-const launchscreenBg = require("../../assets/launchscreen-bg.png");
+const launchscreenBg = require("../../assets/degoas-francella-flip.png");
 const launchscreenLogo = require("../../assets/logo-kitchen-sink.png");
+const deviceHeight = Dimensions.get("window").height;
 
 class Home extends Component {
   constructor(props) {
@@ -15,28 +16,26 @@ class Home extends Component {
     return (
       <Container>
         <StatusBar barStyle="light-content" />
-        <ImageBackground source={launchscreenBg} style={styles.imageContainer}>
-          <View style={styles.logoContainer}>
-            <ImageBackground source={launchscreenLogo} style={styles.logo} />
-          </View>
+        <ImageBackground source={launchscreenBg} style={styles.imageContainer}>          
           <View
             style={{
               alignItems: "center",
-              marginBottom: 50,
-              backgroundColor: "transparent"
+              marginBottom: 5,
+              backgroundColor: "transparent",
+              top: deviceHeight / 2
             }}
           >
-            <H3 style={styles.text}>Welcome</H3>
+          <H3 style={styles.text}>Welcome</H3>
             <View style={{ marginTop: 8 }} />
           <H3 style={styles.text}>{this.props.screenProps.user.email ?? ''}</H3>
             <View style={{ marginTop: 8 }} />
           </View>
-          <View style={{ marginBottom: 80 }}>
+          <View style={{ marginBottom: 80, top: deviceHeight / 2 }}>
             <Button
               style={{ backgroundColor: "#6FAF98", alignSelf: "center" }}
               onPress={() => this.props.navigation.openDrawer()}
             >
-              <Text>Lets Go!</Text>
+              <Text>Start Browsing!</Text>
             </Button>
           </View>
         </ImageBackground>
---
+++ app/screens/myPostings/MyOffers.jsx
@@ -0,0 +1,157 @@
+import { Container, Header, Title, Content, Body, Spinner, CardItem, Card, Left, ListItem, Text, Button, View } from 'native-base';
+import React from "react";
+import {get, post} from '../../api/ApiHelper';
+import Constants from 'expo-constants';
+import { Image, Alert } from 'react-native';
+import moment from 'moment';
+
+const postingImage = require("../../assets/degoas.png");
+
+export default class MyOffers extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+                offers:[],
+                fetching: true
+        }
+        this.rejectOffer = this.rejectOffer.bind(this)
+        this.acceptOffer = this.acceptOffer.bind(this)
+      }
+
+      async getOffers(){
+        let offersResponse = await get(Constants.manifest.extra.myOffersEndpoint, this.props.screenProps.user.accessToken)
+        if(offersResponse.status == 200){
+          let json = await offersResponse.json();          
+          const postingsWithImagesResponse = await Promise.all(json.message.map(async (i) => {
+            let imagesResponse = await get(Constants.manifest.extra.imagesEndpoint + i.id_posting, this.props.screenProps.user.accessToken)
+            if(imagesResponse.status == 200){
+            let jsonImg = await imagesResponse.json();
+            if(Array.isArray(jsonImg.message) && jsonImg.message.length > 0){              
+            return {...i, image: {uri: jsonImg.message.map(x => x.url)[0]}}
+          }else{
+            return {...i, image: postingImage} 
+          }          
+        }else{       
+          return {...i, image: postingImage}   
+        }
+          }));
+          this.setState({offers: postingsWithImagesResponse})          
+          this.setState({fetching: false})
+        }else{
+          let json = await postingsResponse.json();
+          this.setState({error: json.message ?? 'Sorry. Could not retrieve postings.'});
+        }
+      }
+
+      async componentDidMount(){
+        this.getOffers()
+      }      
+
+      componentDidUpdate(prevProps, prevState, snapshot){        
+        if(prevProps.navigation !== this.props.navigation){
+          if(this.props.navigation.getParam('refresh')){
+            this.setState({fetching: true})
+            this.getOffers();
+            this.props.navigation.setParams({refresh: false})
+          }          
+        }   
+      }
+
+      async acceptOffer(transactionHash, index){
+        const body = {transactionHash: transactionHash}        
+        let response = await post(Constants.manifest.extra.acceptBookingEndpoint, body, this.props.screenProps.user.accessToken)
+        if(response.status == 200){
+            Alert.alert(
+                "Offer Accepted Successfully.",
+                "",
+                [              
+                  { text: "OK"}
+                ],
+                { cancelable: false }
+              ); 
+            let newOffers = this.state.offers.filter((_, i) => i !== index)
+            this.setState({offers: newOffers})
+              
+        }else{
+            let json = await response.json();
+            Alert.alert(
+                json.message ?? "Something went wrong. Please try again later.",
+                "",
+                [              
+                  { text: "OK"}
+                ]                
+              ); 
+        }
+      }
+
+      async rejectOffer(transactionHash, index){
+        const body = {transactionHash: transactionHash}          
+        let response = await post(Constants.manifest.extra.rejectBookingEndpoint, body, this.props.screenProps.user.accessToken)
+        if(response.status == 200){
+            Alert.alert(
+                "Offer Rejected Successfully.",
+                "",
+                [              
+                  { text: "OK"}
+                ]
+              ); 
+            let newOffers = this.state.offers.filter((_, i) => i !== index)
+            this.setState({offers: newOffers})
+        }else{
+            let json = await response.json();           
+            Alert.alert(
+                json.message ?? "Something went wrong. Please try again later.",
+                "",
+                [              
+                  { text: "OK"}
+                ]                
+              ); 
+        }
+      }
+
+  render() {
+    return <Container>
+        <Header>
+        <Body style={{flex:1,justifyContent: "center",alignItems: "center"}}>
+            <Title>My Offers</Title>
+          </Body>
+        </Header>
+        <Content>            
+            { this.state.fetching && <Spinner color='blue' />}
+            { !this.state.fetching && this.state.offers.map((offer,index) => (
+             <ListItem key={'posting-' + offer.id_posting + '_' + index}>
+            <Card style={{flex: 1}}>
+            <CardItem style={{flex: 1}}>
+              <Left>                
+                <Body>
+                  <Text>{offer.first_name_booker && offer.last_name_booker ? 'Offer from: ' + offer.first_name_booker + ' ' + offer.last_name_booker : ''}</Text>                  
+                </Body>
+              </Left>
+            </CardItem>
+            <CardItem>
+              <Left>
+                <Text>From: {moment(Date.parse(offer.start_date)).format('YYYY-MM-DD')}</Text>
+                <Text>To: {moment(Date.parse(offer.end_date)).format('YYYY-MM-DD')}</Text>                
+              </Left>
+            </CardItem>
+            <CardItem>
+              <Body>
+                <Image source={offer.image ?? postingImage}  style={{width: '100%', height: 200, resizeMode: 'contain',flex: 1}} />
+                <Button primary style={{ alignSelf: "center", marginBottom:10, width:200, marginTop:20,backgroundColor: "#00a000" }} onPress={() => this.acceptOffer(offer.transaction_booking, index)}>
+                    <View style={{flex:1,justifyContent: "center",alignItems: "center"}}>
+                        <Text style={{color:'white'}}>Accept</Text>
+                    </View>
+                </Button>
+                <Button primary style={{ alignSelf: "center", marginBottom:10, width:200, marginTop:10,backgroundColor: "#C83200" }} onPress={() => this.rejectOffer(offer.transaction_booking, index)}>
+                    <View style={{flex:1,justifyContent: "center",alignItems: "center"}}>
+                        <Text style={{color:'white'}}>Reject</Text>
+                    </View>
+                </Button>
+              </Body>
+            </CardItem>            
+          </Card>
+          </ListItem> ))}
+        </Content>
+    </Container>;
+  }
+}
